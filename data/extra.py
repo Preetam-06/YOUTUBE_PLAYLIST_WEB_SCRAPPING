b0d2f285c533fa0f345a7fb2dc4afc69432cb064
Preetam Omnarayan Prajapati <pritamop21@gmail.com>
2026-05-27 22:04:06 +0530

    added extra.py consisting future updates as for other countries

--- data/extra.py
+++ data/extra.py
@@ -1,7 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-
 billboard_hotw_urls = {
     # Asia & Asia-Pacific
     "india": "https://www.billboard.com/charts/india-songs-hotw/",
@@ -56,19 +52,3 @@ billboard_hotw_urls = {
     "turkey": "https://www.billboard.com/charts/turkey-songs-hotw/",
     "uae": "https://www.billboard.com/charts/united-arab-emirates-songs-hotw/"
 }
-url = "https://www.billboard.com/charts/pakistan-songs-hotw/"
-def get_top_songs(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    #print(soup.prettify())
-    songs = []
-    raw_songs = soup.select("li h3[id='title-of-a-story']")
-
-    for song in raw_songs:
-        songs.append(song.get_text(strip=True))
-
-    return songs
-
-songs_list = get_top_songs(url)
-print(songs_list)
